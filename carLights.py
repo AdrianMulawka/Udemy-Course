d95b32739e56e70266322f79ef6e286067c0bc47
AdrianMulawka <adrian.mulawka@wp.pl>
2019-02-16 11:59:00 +0100

    Add second file: Car Lights

---
+++ carLights.py
@@ -0,0 +1,13 @@
+isAutomaticMode = True
+is80PercentLight = True
+isDirectLight = False
+isRainy = True
+
+turnLightsOn = isAutomaticMode and ( not isDirectLight or isRainy or is80PercentLight )
+print(turnLightsOn)
+
+print("Automatic mode:   ", isAutomaticMode)
+print("Is the light good:", is80PercentLight)
+print("Is sun low:       ", isDirectLight)
+print("Is it rainy:      ", isRainy)
+print("TURN LIGHTS ON:   ", turnLightsOn)
